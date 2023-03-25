b2cd803e16655ea368a999c4ea236f5ae8735d5e
zerbeln <zerbeln@oregonstate.edu>
2023-03-24 19:04:32 -0700

    Modified manual solution finder

--- run_gridworld.py
+++ run_gridworld.py
@@ -1,45 +1,39 @@
 from gridworld import GridWorld
 
 
-def manual_gridworld():
+def manual_gridworld(gw):
     """
     This is a manually written gridworld solver to test environmental mechanics
     """
-    width = 5
-    height = 5
-    n_agents = 1
-    n_targets = 1
-
-    gw = GridWorld(width, height)
-    gw.create_world(n_agents, n_targets)
 
     # Testing environment mechanics with manual strategy
     x_dist = gw.targets[0][0] - gw.agents['A0'].loc[0]
     y_dist = gw.targets[0][1] - gw.agents['A0'].loc[1]
 
-    reward = 0
-    while x_dist != 0:
-        if x_dist < 0:
-            action = 2
-            reward, gw.agents['A0'].loc = gw.step(gw.agents['A0'].loc, action)
-        else:
-            action = 3
-            reward, gw.agents['A0'].loc = gw.step(gw.agents['A0'].loc, action)
-        x_dist = gw.targets[0][0] - gw.agents['A0'].loc[0]
-
+    solution = []
     while y_dist != 0:
         if y_dist > 0:
             action = 0
+            solution.append(action)
             reward, gw.agents['A0'].loc = gw.step(gw.agents['A0'].loc, action)
         else:
             action = 1
+            solution.append(action)
             reward, gw.agents['A0'].loc = gw.step(gw.agents['A0'].loc, action)
         y_dist = gw.targets[0][1] - gw.agents['A0'].loc[1]
 
-    x_dist = gw.targets[0][0] - gw.agents['A0'].loc[0]
-    y_dist = gw.targets[0][1] - gw.agents['A0'].loc[1]
-    print("Total distance: ", x_dist + y_dist)
-    print("Reward is: ", reward)
+    while x_dist != 0:
+        if x_dist < 0:
+            action = 2
+            solution.append(action)
+            reward, gw.agents['A0'].loc = gw.step(gw.agents['A0'].loc, action)
+        else:
+            action = 3
+            solution.append(action)
+            reward, gw.agents['A0'].loc = gw.step(gw.agents['A0'].loc, action)
+        x_dist = gw.targets[0][0] - gw.agents['A0'].loc[0]
+
+    return solution
 
 
 if __name__ == "__main__":
